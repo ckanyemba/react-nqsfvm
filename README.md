5ceef98d43ac5b109017e7583960f4160f9d5baf
ckanyemba <kanyembacraig0@gmail.com>
2022-08-08 01:58:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-nqsfvm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-nqsfvm)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+
+      <h2>Hello StackBlitz!</h2>
+      <p1>Start editing to see some magic happen :)</p1>
+    </div>
+  );
+}
